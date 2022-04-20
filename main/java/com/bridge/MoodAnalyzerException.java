b9bb71fbd50a75eadd10a30bbdb9800389715056
Karthik <jeefkarthik036@gmail.com>
2022-04-20 21:26:55 +0530

    second-Added UC13_LambdaExprsns

---
+++ main/java/com/bridge/MoodAnalyzerException.java
@@ -0,0 +1,18 @@
+package com.bridge;
+
+public class MoodAnalyzerException extends Exception {
+
+    public String message;
+    static ExceptionType type;
+
+    enum ExceptionType {
+        EMPTY, NULL;
+    }
+
+
+    public MoodAnalyzerException(ExceptionType type, String message) {
+        this.message = message;
+        this.type = type;
+    }
+
+}
